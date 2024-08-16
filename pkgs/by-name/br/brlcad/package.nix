a6b403cbc88cfd2fd0bba3ab0b51560ad5e1703d
Gaetan Lepage <gaetan@glepage.com>
2024-08-16 22:26:27 +0200

    brlcad: 7.38.2 -> 7.40.2
    Diff: https://github.com/BRL-CAD/brlcad/compare/rel-7-38-2...rel-7-40-2
    
    Changelog: https://github.com/BRL-CAD/brlcad/releases/tag/rel-7-40-2

--- pkgs/by-name/br/brlcad/package.nix
+++ pkgs/by-name/br/brlcad/package.nix
@@ -4,6 +4,7 @@
   fetchFromGitHub,
   fetchpatch,
   cmake,
+  git,
   fontconfig,
   libX11,
   libXi,
@@ -11,28 +12,40 @@
   mesa,
 }:
 
+let
+  bext = fetchFromGitHub {
+    owner = "BRL-CAD";
+    repo = "bext";
+    rev = "";
+    hash = "";
+  };
+in
+
 stdenv.mkDerivation rec {
   pname = "brlcad";
-  version = "7.38.2";
+  version = "7.40.2";
 
   src = fetchFromGitHub {
     owner = "BRL-CAD";
     repo = "brlcad";
     rev = "refs/tags/rel-${lib.replaceStrings [ "." ] [ "-" ] version}";
-    hash = "sha256-23UTeH4gY2x/QGYZ64glAkf6LmsXBAppIOHgoUdxgpo=";
+    hash = "sha256-v2MxatzplBpZ4Y/PAlAxAe3uKu8HboBu1+WM0R4yTwo=";
   };
 
-  patches = [
-    # This commit was bringing an impurity in the rpath resulting in:
-    # RPATH of binary /nix/store/rq2hjvfgq2nvh5zxch51ij34rqqdpark-brlcad-7.38.0/bin/tclsh contains a forbidden reference to /build/
-    (fetchpatch {
-      url = "https://github.com/BRL-CAD/brlcad/commit/fbdbf042b2db4c7d46839a17bbf4985cdb81f0ae.patch";
-      revert = true;
-      hash = "sha256-Wfihd7TLkE8aOpLdDtYmhhd7nZijiVGh1nbUjWr/BjQ=";
-    })
-  ];
+  # patches = [
+  #   # This commit was bringing an impurity in the rpath resulting in:
+  #   # RPATH of binary /nix/store/rq2hjvfgq2nvh5zxch51ij34rqqdpark-brlcad-7.38.0/bin/tclsh contains a forbidden reference to /build/
+  #   (fetchpatch {
+  #     url = "https://github.com/BRL-CAD/brlcad/commit/fbdbf042b2db4c7d46839a17bbf4985cdb81f0ae.patch";
+  #     revert = true;
+  #     hash = "sha256-Wfihd7TLkE8aOpLdDtYmhhd7nZijiVGh1nbUjWr/BjQ=";
+  #   })
+  # ];
 
-  nativeBuildInputs = [ cmake ];
+  nativeBuildInputs = [
+    cmake
+    git
+  ];
 
   buildInputs = [
     fontconfig
@@ -49,13 +62,16 @@ stdenv.mkDerivation rec {
     "-Wno-error=array-bounds"
   ];
 
-  meta = with lib; {
+  meta = {
     homepage = "https://brlcad.org";
     description = "BRL-CAD is a powerful cross-platform open source combinatorial solid modeling system";
     changelog = "https://github.com/BRL-CAD/brlcad/releases/tag/${lib.removePrefix "refs/tags/" src.rev}";
-    license = with licenses; [ lgpl21 bsd2 ];
-    maintainers = with maintainers; [ GaetanLepage ];
-    platforms = platforms.linux;
+    license = with lib.licenses; [
+      lgpl21
+      bsd2
+    ];
+    maintainers = with lib.maintainers; [ GaetanLepage ];
+    platforms = lib.platforms.linux;
     # error Exactly one of ON_LITTLE_ENDIAN or ON_BIG_ENDIAN should be defined.
     broken = stdenv.system == "aarch64-linux";
   };
