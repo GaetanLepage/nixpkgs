e4bb4106fbc20c999e2e344d95a23e402f52ec0e
Gaetan Lepage <gaetan@glepage.com>
2024-10-18 13:17:47 +0200

    python312Packages.deepwave: 0.0.18 -> 0.0.20
    Diff: https://github.com/ar4/deepwave/compare/v0.0.18...v0.0.20

--- pkgs/development/python-modules/deepwave/default.nix
+++ pkgs/development/python-modules/deepwave/default.nix
@@ -1,73 +1,77 @@
 {
   lib,
+  config,
   buildPythonPackage,
   fetchFromGitHub,
+
+  # nativeBuildInputs
+  cudaPackages,
+
+  # dependencies
   torch,
-  ninja,
+  pybind11,
+
+  # tests
+  pytestCheckHook,
   scipy,
   which,
-  pybind11,
-  pytest-xdist,
-  pytestCheckHook,
+
+  cudaSupport ? config.cudaSupport,
 }:
 
-let
-  linePatch = ''
-    import os
-    os.environ['PATH'] = os.environ['PATH'] + ':${ninja}/bin'
-  '';
-in
 buildPythonPackage rec {
   pname = "deepwave";
-  version = "0.0.18";
-  format = "pyproject";
+  version = "0.0.20";
+  pyproject = true;
 
   src = fetchFromGitHub {
     owner = "ar4";
     repo = "deepwave";
-    rev = "v${version}";
-    hash = "sha256-DOOy+B12jgwJzQ90qzX50OFxYLPRcVdVYSE5gi3pqDM=";
+    tag = "v${version}";
+    hash = "sha256-AZmh99BLa/A9Jvxk9O14BlKoIGc2qKDiA1A956RmsvA=";
   };
 
-  # unable to find ninja although it is available, most likely because it looks for its pip version
   postPatch = ''
-    substituteInPlace setup.cfg --replace "ninja" ""
+    substituteInPlace src/deepwave/build_linux.sh \
+      --replace-fail " -Ofast -mavx2" ""
 
-    # Adding ninja to the path forcibly
-    mv src/deepwave/__init__.py tmp
-    echo "${linePatch}" > src/deepwave/__init__.py
-    cat tmp >> src/deepwave/__init__.py
-    rm tmp
+    patchShebangs .
   '';
 
-  # The source files are compiled at runtime and cached at the
-  # $HOME/.cache folder, so for the check phase it is needed to
-  # have a temporary home. This is also the reason ninja is not
-  # needed at the nativeBuildInputs, since it will only be used
-  # at runtime.
+  nativeBuildInputs = lib.optionals cudaSupport [
+    cudaPackages.cuda_nvcc
+  ];
+
+  buildInputs = lib.optionals cudaSupport [
+    cudaPackages.cuda_cudart.dev
+  ];
+
   preBuild = ''
-    export HOME=$(mktemp -d)
+    pushd src/deepwave
+    ./build_linux.sh ${lib.optionalString (!cudaSupport) "NOCUDA"}
+    popd
   '';
 
-  propagatedBuildInputs = [
+  dependencies = [
     torch
     pybind11
   ];
 
   nativeCheckInputs = [
-    which
-    scipy
-    pytest-xdist
     pytestCheckHook
+    scipy
+    which
   ];
+
+  pytestFlagsArray = [ "-vvvvv" ];
 
   pythonImportsCheck = [ "deepwave" ];
 
-  meta = with lib; {
+  meta = {
     description = "Wave propagation modules for PyTorch";
     homepage = "https://github.com/ar4/deepwave";
-    license = licenses.mit;
-    platforms = intersectLists platforms.x86_64 platforms.linux;
-    maintainers = with maintainers; [ atila ];
+    license = lib.licenses.mit;
+    platforms = lib.intersectLists lib.platforms.x86_64 lib.platforms.linux;
+    maintainers = with lib.maintainers; [ atila ];
   };
 }
