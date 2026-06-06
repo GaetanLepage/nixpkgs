63cfb5e8f19173f8ffa6b651eff32ac1e0205b7c
Gaetan Lepage <gaetan@glepage.com>
2026-06-06 11:59:07 +0000

    cudaPackages_13_3.cuda_cccl: alias to cccl

--- pkgs/development/cuda-modules/aliases.nix
+++ pkgs/development/cuda-modules/aliases.nix
@@ -6,7 +6,16 @@ let
     { path, package }:
     lib.warn "cudaPackages.${oldName} is deprecated, use ${path} instead" package;
 in
-final: _:
-builtins.mapAttrs mkRenamed {
+final: prev:
+(builtins.mapAttrs mkRenamed {
   # A comment to prevent empty { } from collapsing into a single line
+})
+// {
+  cuda_cccl =
+    if (prev.cudaAtLeast "13.3") then
+      lib.warn "cudaPackages.cuda_cccl is deprecated since CUDA 13.3, use cudaPackages.cccl instead" final.cccl
+    else
+      # Do not warn when running a CUDA version where cccl is still named cuda_cccl
+      # -> Just alias silently
+      final.cccl;
 }
